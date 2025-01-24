508060467e46e1934c186eb40ebe182b53d4d536
Devcodino <devcodino@proton.me>
2025-01-24 14:08:09 +0100

    Firebase Settings hinzugefügt

---
+++ src/firebase/firebase.settings.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDDujDey0JtHMJY7tTEuCMdCjVWmiOCpvk",
+    authDomain: "test-cc739.firebaseapp.com",
+    projectId: "test-cc739",
+    storageBucket: "test-cc739.firebasestorage.app",
+    messagingSenderId: "542943429717",
+    appId: "1:542943429717:web:4fe33a33701fe3d1eb00dc"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
